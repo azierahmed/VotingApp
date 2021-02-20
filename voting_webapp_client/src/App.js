268e5f3bc459b193dd7ffd896ab6701ef47b1cc9
Tareq Aljamal <tareqjj@gmail.com>
2021-02-20 14:12:06 +0200

    initial project strecture

---
+++ voting_webapp_client/src/App.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+import VoteWizard from "./compnents/VoteWizard/VoteWizard";
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
